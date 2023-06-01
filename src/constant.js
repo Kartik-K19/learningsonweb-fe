63e0752df11ee93a8cec2eb4ae8e74171a80f62d
Kartik <kartiklonkar@gmail.com>
2023-06-01 18:38:07 +0530

    env commit

--- src/constant.js
+++ src/constant.js
@@ -2,5 +2,5 @@
 export const API = process.env.REACT_APP_API;
 export const AUTH_TOKEN = "authToken";
 export const BEARER = "Bearer";
-export const BACKEND_URL = process.env.REACT_APP_BACKEND_URL
+export const BACKEND_URL ='';
 export const REACT_APP_STRIPE_PUBLISHABLE_KEY = process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY
